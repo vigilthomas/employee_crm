9b5e9ea5fb574aefa86cfb73d8d3448cf3db08c9
vigilthomas17 <vigilthomas17@gmail.com>
2023-10-30 13:44:19 +0530

    Adding files

---
+++ crm/models.py
@@ -0,0 +1,10 @@
+from django.db import models
+
+# Create your models here.
+class Employees(models.Model):
+    name=models.CharField(max_length=100)
+    department=models.CharField(max_length=100)
+    salary=models.PositiveBigIntegerField(max_length=100)
+    email=models.EmailField(unique=True)
+    age=models.PositiveBigIntegerField(null=True)
+    phone=models.IntegerField(max_length=10)
